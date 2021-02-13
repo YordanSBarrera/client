e1209a0a924af899528031f97702ba0e53e1a490
Yordan SB <yordansaldanabarrera@gmail.com>
2021-02-12 23:25:08 -0300

    guardando trabajo

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,26 +1,68 @@
 import React from 'react'
 
-class GoogleAuth extends React.Component{
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null }
 
-    componentDidMount(){
-        window.gapi.load('client:auth2',()=>{
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId:'958107640304-skghhbvovt70r9ul8hfhhlp4eio064ro.apps.googleusercontent.com',
-                scope:'email'
+                clientId: '958107640304-skghhbvovt70r9ul8hfhhlp4eio064ro.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         })
-     /*   958107640304-skghhbvovt70r9ul8hfhhlp4eio064ro.apps.googleusercontent.com
-        CLIENT ID
-        
-        lIiLw3gq7fkIj3wo-0EQxojI
-        Client Secret
-        */
+        /*   958107640304-skghhbvovt70r9ul8hfhhlp4eio064ro.apps.googleusercontent.com
+           CLIENT ID
+           
+           lIiLw3gq7fkIj3wo-0EQxojI
+           Client Secret
+           */
     }
 
-    render(){
-        return(
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        }
+        if (this.state.isSignedIn) {
+            return (
+                <button 
+                className="ui red google button"
+                onClick={this.onSignOutClick}
+                >
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            )
+        } else {
+            return (
+                <button 
+                className="ui red google button"
+                onClick={this.onSignInClick}
+                >
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            )
+        }
+    }
+
+    render() {
+        return (
             <div>
-                GoogleAuth
+                {this.renderAuthButton()}
             </div>
         )
     }
